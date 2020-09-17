c5c9c16628b4f06163e203614d0fbef169c7e7b9
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2020-09-17 19:48:46 +0300

    simplification

--- src/tensors.jl
+++ src/tensors.jl
@@ -82,23 +82,18 @@ function coulomb_tensor(ρ, transtensor, gpoints, wgp, t, wt)
     @assert length(t) == length(wt)
 
     # Coulomb tensor (returned at end)
-    ν = similar(ρ)
-    ν .= 0
+    V = similar(ρ)
+    V .= 0
 
     # Initializing temporary tensors
     T = similar(transtensor, size(transtensor)[1:end-1])
-    d = similar(ρ)
-    e = similar(ρ)
     v = similar(ρ)
 
     @showprogress "Calculating v-tensor..." for p in Iterators.reverse(eachindex(wt))
         T = transtensor[:,:,:,:,p]
-        @tensoropt begin
-            d[α',β',γ, I',J',K] = T[γ,γ',K,K']*ρ[α',β',γ',I',J',K'];
-            e[α',β,γ,I',J,K] = d[α',β',γ,I',J',K]*T[β,β',J,J'];
-            v[α,β,γ,I,J,K] = e[α',β,γ,I',J,K]*T[α,α',I,I'];
-        end
-        ν = ν .+ wt[p].*v
+        @tensoropt v[α,β,γ,I,J,K] = T[α,α',I,I']*T[β,β',J,J']*T[γ,γ',K,K']*ρ[α',β',γ',I',J',K']
+
+        V = V .+ wt[p].*v
     end
-    return ν
+    return V
 end
