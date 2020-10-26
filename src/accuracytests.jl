c9a11cc87968247e9148e92cce7a24a317bd00a9
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2020-10-26 20:26:58 +0200

    Documentation and delete old stuff

--- src/accuracytests.jl
+++ src/accuracytests.jl
@@ -11,86 +11,28 @@ function gaussiandensity_self_energy(rtol=1e-12; tmin=0, tmax=Inf)
     return quadgk(_f, tmin, tmax, rtol=rtol)
 end
 
-
 """
-    test_accuracy(n_elements, n_gaussp, n_tpoints; Keywords) -> Float
+    test_accuracy(a::Real, ne::Int, ng::Int, nt::Int; kwords) -> Float64
 
-Calculates self-energy for Gaussian change density numerically and compares it analytic result.
+Test accuracy on Gaussian charge distribution self energy.
 
 # Arguments
-- `n_elements`  : Number of finite elements in calculation
-- `n_gaussp`    : Number of Gauss points in element per degree of freedom
-- `n_tpoins`    : Number of Gauss points for t-variable integration
+- `a::Real`   : Simulation box size
+- `ne::Int`   : Number of elements per dimension
+- `ng::Int`   : Number of Gausspoints per dimension for r
+- `nt::Int`   : Number of Gausspoints per dimension for t
 
 # Keywords
-- `tmax=20`          : Maximum value in t integration
-- `tmin=0`           : Minimum value in t integration
-- `correction=true`  : Add correction to electric field calculation
-- `cmax=10`          : Limits for numerical calculation - cubic box from `-cmax` to `cmax`
-- `mode=:normal`     : Integration method `:normal`,  `:normal_alt` or `:harrison`
-- `δ=1.0`            : Area parameter to determine average value in `:normal_alt` δ∈]0,1]
-- `μ=0`              : Parameter for `:harrison` mode
+- `tmax=25`      : Maximum t-value for integration
+- `tmin=0`       : Minimum t-value for integration
+- `mode=:normal` : Integration type - options `:normal`, `:log`, `loglocal`, `:local` and `:combination`
+- `δ=0.25`       : Localization parameter for local integration types
+- `correction=true`   : Correction to `tmax`-> ∞ integration - `true` calculate correction, `false` do not calculate
+- `tboundary=20`      : Parameter for `:combination` mode. Switch to `:loglocal` for t>`tboundary` else use `:log`
 """
-function test_accuracy(n_elements, n_gaussp, n_tpoints;
-                       tmax=20,
-                       tmin=0,
-                       correction=true,
-                       cmax=10,
-                       mode=:normal,
-                       δ=1.0,
-                       μ=0)
-    @info "Initializing elements and Gauss points"
-    eq = CubicElements(2cmax, n_elements)
-
-    x, w = gausspoints(eq, n_gaussp)
-    centers = getcenters(eq)
-
-    @info "Generating transformation tensor"
-    if mode in [:alt, :normal_alt]
-        @info "Mode is normal with local mean values for T-tensor"
-        T, t, wt = transformation_tensor_alt(eq, x, w, n_tpoints; δ=δ, tmin=tmin)
-    elseif mode == :normal
-        @info "Mode is normal"
-        T, t, wt = transformation_tensor(centers, x, w, n_tpoints; tmax=tmax, tmin=tmin)
-    elseif mode == :harrison
-        @info "Mode is Harrison"
-        T, t, wt = transformation_harrison(eq, x, w, n_tpoints; tmax=tmax, μ=μ)
-    elseif mode == :harrison_alt
-        @info "Mode is Harrison with local mean values for T-tensor"
-        T, t, wt = transformation_harrison_alt(eq, x, w, n_tpoints; tmax=tmax, δ=δ, μ=μ)
-    else
-        @warn "Mode not recognised. Using normal mode"
-        T, t, wt = transformation_tensor(centers, x, w, n_tpoints; tmax=tmax, tmin=tmin)
-    end
-    @info "Generating electron density tensor"
-
-
-    ρ = density_tensor_old(centers, x)
-    V = coulomb_tensor(ρ, T, x, w, t, wt)
-
-
-    if correction
-        V = V .+ (π/tmax^2).*ρ   # add correction
-    end
-
-
-
-
-    # Integraton weights fore elements + Gausspoints in tersor form
-    ω = hcat([Array(w) for i in 1:n_elements]...)
-
-    cc = V.*ρ
-    E = @tensor ω[α,I]*ω[β,J]*ω[γ,K]*cc[α,β,γ,I,J,K]
-    E_true = 21.92474849998632 # = gaussiandensity_self_energy()[1]
-    @info "Calculated energy = $E"
-    @info "True energy = $E_true"
-    @info "Error = $(E-E_true) ; error/E = $( round((E-E_true)/E_true; sigdigits=1))"
-    return E, E-E_true
-end
-
-
-function test_accuracy_new(a::Real, ne::Int, ng::Int, nt::Int;
-     tmax=25, tmin=0, mode=:normal, ae=1.0, δ=0.25, correction=true)
+function test_accuracy(a::Real, ne::Int, ng::Int, nt::Int;
+     tmax=25, tmin=0, mode=:normal, δ=0.25, correction=true, tboundary=20)
+    ae=1.0
     ceg = CubicElementGrid(a, ne, ng)
     if mode == :normal
         @info "Normal mode"
@@ -104,13 +46,16 @@ function test_accuracy_new(a::Real, ne::Int, ng::Int, nt::Int;
     elseif mode == :loglocal
         @info "Log local mode"
         ct = CoulombTransformationLogLocal(ceg, nt; tmax=tmax, tmin=tmin, δ=δ)
+    elseif mode == :combination
+        @info "Combination mode"
+        ct = loglocalct(ceg, nt; tmax=tmax, δ=δ, tboundary=tboundary)
     else
         error("Mode not known")
     end
-    test_accuracy_new(ceg, ct; correction=correction, ae=ae)
+    test_accuracy(ceg, ct; correction=correction, ae=ae)
 end
 
-function test_accuracy_new(ceg::CubicElementGrid, ct::AbstractCoulombTransformation;
+function test_accuracy(ceg::CubicElementGrid, ct::AbstractCoulombTransformation;
                             ae=1.0, correction=true)
     tmin = ct.tmin
     tmax = ct.tmax
@@ -132,8 +77,8 @@ function test_accuracy_new(ceg::CubicElementGrid, ct::AbstractCoulombTransformat
     @info "Total Energy (reference) = $E_tot"
     @info "Integration error = $(round((E_int-E_true); sigdigits=2)) ; error/E = $( round((E_int-E_true)/E_true; sigdigits=2))"
     @info "Integration error relative to total energy = $( round((E_int-E_true)/E_tot; sigdigits=2))"
-    @info "Tail energy = $(round(E_tail; sigdigits=2)) ; E_tail/E_tot = $(round(E_tail/E_tot; sigdigits=2))"
-    @info "Energy correction = $(round(E_cor; sigdigits=2))  ; (E_cor-E_tail)/E = $(round((E_cor-E_tail)/E_tot;sigdigits=2))"
+    @info "Tail energy = $(round(E_tail; sigdigits=5)) ; E_tail/E_tot = $(round(E_tail/E_tot; sigdigits=2))"
+    @info "Energy correction = $(round(E_cor; sigdigits=5))  ; (E_cor-E_tail)/E_tot = $(round((E_cor-E_tail)/E_tot;sigdigits=2))"
     @info "Error to total energy error/E_tot = $(round((E-E_tot)/E_tot; sigdigits=2))"
     return E
 end
--- src/tensors.jl
+++ src/tensors.jl
@@ -129,6 +129,18 @@ function CoulombTransformationCombination(t...)
     return ct
 end
 
+function loglocalct(ceg::CubicElementGrid, nt::Int; δ=0.25, tmax=300, tboundary=20, ε=1e-12)
+    @assert 0 < tboundary < tmax
+    s, ws = gausspoints(nt; elementsize=(log(ε), log(tmax)))
+    t = exp.(s)
+    l = length(t[t .< tboundary])
+    ct1 = CoulombTransformationLog(ceg, l, tmax=tboundary, ε=ε)
+    ct2 = CoulombTransformationLogLocal(ceg, nt-l; tmin=tboundary, tmax=tmax, δ=δ)
+    return CoulombTransformationCombination(ct1,ct2)
+end
+
+
+
 (ct::AbstractCoulombTransformation)(r,t) = exp(-(t*r)^2)
 (ct::AbstractCoulombTransformationSingle)(r,p::Int) = exp(-ct.t[p]^2*r^2)
 
@@ -207,6 +219,11 @@ function Base.show(io::IO, ::MIME"text/plain", cct::CoulombTransformationCombina
     print(io, "Coulomb transformation combination tensor size=$(size(cct))")
 end
 
+function Base.show(io::IO, ::MIME"text/plain", cct::CoulombTransformationLogLocal)
+    print(io, "Coulomb transformation logarimic local size=$(size(cct))")
+end
+
+
 function ω_tensor(ceg::CubicElementGrid)
     n = size(ceg)[end]
     l = length(ceg.w)
@@ -222,228 +239,12 @@ end
 
 
 
-function transformation_tensor(elements, gpoints, w, nt::Int; tmax=20, tmin=0)
-    @assert length(w) == length(gpoints)
-    T = similar(gpoints,
-        length(gpoints),
-        length(gpoints),
-        length(elements),
-        length(elements),
-        nt
-    )
-    t, wt = gausspoints(nt; elementsize=(tmin,tmax))
-    for p ∈ eachindex(t)
-        for (I,J) ∈ Iterators.product(eachindex(elements), eachindex(elements))
-            for β ∈ eachindex(gpoints)
-                for α ∈ eachindex(gpoints)
-                    T[α,β,I,J,p] = w[β].*exp.(
-                        -t[p]^2*(gpoints[α]+elements[I]-gpoints[β]-elements[J])^2
-                    )
-                end
-            end
-        end
-    end
-    return T, t, wt
-end
-
-function transformation_tensor(elements::CubicElements, gpoints, w, nt::AbstractVector, borders::AbstractVector )
-    @assert length(nt) == length(borders)
-    boff = OffsetArray( vcat([0], borders), 0:length(borders))
-    T = []
-    t = []
-    wt = []
-    ele = getcenters(elements)
-    for i ∈ eachindex(nt)
-        tT, tt, twt = transformation_tensor(ele, gpoints, w, nt[i]; tmax=boff[i], tmin=boff[i-1])
-        push!(T, tT)
-        push!(t, tt)
-        push!(wt, twt)
-    end
-    return cat(T...; dims=5), vcat(t...), vcat(wt...)
-end
-
-function transformation_tensor_alt(elements::CubicElements, gpoints, w,
-                         nt::AbstractVector, borders::AbstractVector; δ=1)
-    #@assert length(nt) == length(borders)
-    boff = OffsetArray( borders, 0:length(borders)-1)
-    T = []
-    t = []
-    wt = []
-    for i ∈ eachindex(nt)
-        tT, tt, twt = transformation_tensor_alt(elements, gpoints, w, nt[i]; tmax=boff[i], tmin=boff[i-1], δ=δ)
-        push!(T, tT)
-        push!(t, tt)
-        push!(wt, twt)
-    end
-    return cat(T...; dims=5), vcat(t...), vcat(wt...)
-end
-
-
-function transformation_tensor_alt(elements::CubicElements, gpoints, w, nt::Int;
-                                   tmax=20, tmin=0, δ=1)
-    @assert length(w) == length(gpoints)
-    @assert δ > 0
-    T = similar(gpoints,
-        length(gpoints),
-        length(gpoints),
-        elements.npoints,
-        elements.npoints,
-        nt
-    )
-    t, wt = gausspoints(nt; elementsize=(tmin,tmax))
-    s = elementsize(elements)/2
-    ele = getcenters(elements)
-    off = OffsetArray( vcat(-s, gpoints, s), 0:length(gpoints)+1)
-    Threads.@threads for p ∈ eachindex(t)
-        for (I,J) ∈ Iterators.product(eachindex(ele), eachindex(ele))
-            for β ∈ 1:length(gpoints)
-                for α ∈ 1:length(gpoints)
-                    r = off[β] - off[α] + ele[J] - ele[I]
-                    βp = 0.5*(off[β+1] - off[β])
-                    βm = 0.5*(off[β-1] - off[β])
-                    αp = 0.5*(off[α+1] - off[α])
-                    αm = 0.5*(off[α-1] - off[α])
-                    rmax = (r + δ*maximum(abs, (βp - αm, βm - αp) ))*t[p]
-                    rmin = (r - δ*minimum(abs, (βp - αm, βm - αp) ))*t[p]
-                    T[α,β,I,J,p] = w[β] * 0.5*√π*erf(rmin, rmax)/(rmax - rmin)
-                end
-            end
-        end
-    end
-    return T, t, wt
-end
-
-
-function transformation_harrison_alt(elements::CubicElements, gpoints, w, nt::Int;
-                                tmax=10, μ=1, δ=1)
-    @assert length(w) == length(gpoints)
-    @assert tmax > 0
-    @assert δ > 0
-    T = similar(gpoints,
-        length(gpoints),
-        length(gpoints),
-        elements.npoints,
-        elements.npoints,
-        nt
-    )
-    ele = getcenters(elements)
-    s, ws = gausspoints(nt; elementsize=(-log(tmax),log(tmax)))
-    ww = similar(ws)
-    es = elementsize(elements)/2
-    off = OffsetArray( vcat(-es, gpoints, es), 0:length(gpoints)+1)
-    Threads.@threads for p ∈ eachindex(s)
-        ww[p] = exp(-0.25μ^2*exp(-2s[p]))
-        for (I,J) ∈ Iterators.product(eachindex(ele), eachindex(ele))
-            for β ∈ 1:length(gpoints)
-                for α ∈ 1:length(gpoints)
-                    r = off[β] - off[α] + ele[J] - ele[I]
-                    βp = 0.5*(off[β+1] - off[β])
-                    βm = 0.5*(off[β-1] - off[β])
-                    αp = 0.5*(off[α+1] - off[α])
-                    αm = 0.5*(off[α-1] - off[α])
-                    rmax = (r + δ*maximum(abs, (βp - αm, βm - αp) ))
-                    rmin = (r - δ*minimum(abs, (βp - αm, βm - αp) ))
-
-                    # Mean value of T-tensor in r=0±δr
-                    meanval = erf(rmin*exp(s[p]), rmax*exp(s[p]))/(rmax - rmin)
-                    T[α,β,I,J,p] = w[β] * 0.5*√π*meanval
-                end
-            end
-        end
-    end
-    return T, s, ws.*ww
-end
-
-
-function transformation_harrison(elements::CubicElements, gpoints, w, nt::Int;
-                                tmax=10, μ=1)
-    @assert length(w) == length(gpoints)
-    @assert tmax > 0
-    T = similar(gpoints,
-        length(gpoints),
-        length(gpoints),
-        elements.npoints,
-        elements.npoints,
-        nt
-    )
-    @debug "transformation_harrison" μ tmax
-    ele = getcenters(elements)
-    s, ws = gausspoints(nt; elementsize=(-log(tmax),log(tmax)))
-    ww = similar(ws)
-    Threads.@threads for p ∈ eachindex(s)
-        ww[p] = exp( (-0.25μ^2*exp(-2s[p])+s[p]) )
-        for (I,J) ∈ Iterators.product(eachindex(ele), eachindex(ele))
-            for β ∈ 1:length(gpoints)
-                for α ∈ 1:length(gpoints)
-                    r = gpoints[β] - gpoints[α] + ele[J] - ele[I]
-                    e = exp( -r^2*exp(2s[p]) )
-                    T[α,β,I,J,p] = w[β]*e
-                end
-            end
-        end
-    end
-    return T, s, ws.*ww
-end
-
-
-
-
-function density_tensor_old(elements, gpoints)
-    ρ = similar(elements,
-        length(gpoints),
-        length(gpoints),
-        length(gpoints),
-        length(elements),
-        length(elements),
-        length(elements)
-    )
-    ne = length(elements)
-    np = length(gpoints)
-    for (I, J, K) ∈ Iterators.product(1:ne, 1:ne, 1:ne)
-        for (i, j) ∈ Iterators.product(1:np, 1:np)
-            ρ[:,i,j,I,J,K] = exp.(
-                -(gpoints .+ elements[I]).^2 .-(gpoints[i]+elements[J])^2 .-(gpoints[j]+elements[K])^2
-            )
-        end
-    end
-    return ρ
-end
-
-
-
 function density_tensor(grid::AbstractArray, r::AbstractVector, a)
     return [ exp(-a*sum( (x-r).^2 )) for x in grid ]
 end
 
 density_tensor(grid; r=SVector(0.,0.,0.), a=1.) = density_tensor(grid, r, a)
 
-
-function density_tensor(elements, gpoints, r::AbstractVector)
-    @assert length(r[1]) == 3
-    ρ = similar(elements,
-        length(gpoints),
-        length(gpoints),
-        length(gpoints),
-        length(elements),
-        length(elements),
-        length(elements)
-    )
-    ρ .= 0
-    ne = length(elements)
-    np = length(gpoints)
-    Threads.@threads for K ∈ 1:ne
-        for (I, J) ∈ Iterators.product(1:ne, 1:ne)
-            for (i, j) ∈ Iterators.product(1:np, 1:np)
-                for xyz ∈ r
-                    ρ[:,i,j,I,J,K] .+= exp.(
-                        -(gpoints .+ elements[I] .- xyz[1]).^2 .-(gpoints[i]+elements[J]-xyz[2])^2 .-(gpoints[j]+elements[K]-xyz[3])^2
-                    )
-                end
-            end
-        end
-    end
-    return ρ
-end
 
 
 function coulomb_tensor(ρ::AbstractArray, transtensor::AbtractTransformationTensor; tmax=nothing)
@@ -477,27 +278,6 @@ function coulomb_tensor(ρ::AbstractArray, transtensor::AbstractArray, wt::Abstr
         V = V .+ coulomb_correction(ρ, tmax)
     end
     return  V
-end
-
-function coulomb_tensor(ρ, transtensor, gpoints, wgp, t, wt)
-    @assert length(gpoints) == length(wgp)
-    @assert length(t) == length(wt)
-
-    # Coulomb tensor (returned at end)
-    V = similar(ρ)
-    V .= 0
-
-    # Initializing temporary tensors
-    #T = similar(transtensor, size(transtensor)[1:end-1])
-    v = similar(ρ)
-
-    @showprogress "Calculating v-tensor..." for p in Iterators.reverse(eachindex(wt))
-        T = @view transtensor[:,:,:,:,p]
-        @tensoropt v[α,β,γ,I,J,K] = T[α,α',I,I']*T[β,β',J,J']*T[γ,γ',K,K']*ρ[α',β',γ',I',J',K']
-
-        V = V .+ wt[p].*v
-    end
-    return V
 end
 
 
