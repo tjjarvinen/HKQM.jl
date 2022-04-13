6d49bddf3059f9aa1ef6759d14419b5f4853584c
Documenter.jl <documenter@juliadocs.github.io>
2022-04-13 20:12:00 +0000

    build based on c80d4bf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#HKQM.jl","page":"HKQM.jl","title":"HKQM.jl","text":"","category":"section"},{"location":"","page":"HKQM.jl","title":"HKQM.jl","text":"DocTestSetup = quote\n    using HKQM\nend","category":"page"},{"location":"","page":"HKQM.jl","title":"HKQM.jl","text":"Documentation for HKQM.jl","category":"page"}]
+}
