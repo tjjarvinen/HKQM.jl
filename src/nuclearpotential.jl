6e44abe4f459c57cf75cc65186ce269fef254192
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2023-05-15 18:39:40 +0300

    clean code

--- src/nuclearpotential.jl
+++ src/nuclearpotential.jl
@@ -396,13 +396,7 @@ function Base.getindex(pt::PotentialTensor, i::Int, j::Int, k::Int, I::Int, J::I
     return out
 end
 
-# function Base.Array(pt::PotentialTensor)
-#     x = Array(pt.x)
-#     y = Array(pt.y)
-#     z = Array(pt.z)
-#     @tullio out[i,j,k,I,J,K] := x[i,I,t] * y[j,J,t] * z[k,K,t] * pt.x.wt[t]
-#     return 2/sqrt(π) .* out
-# end
+
 
 
 """
